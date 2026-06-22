19ee4ad72aab81a6f6d7c809de2e68f84f3246f7
scholar-star <cheve1224@pusan.ac.kr>
2026-06-22 17:40:06 +0900

    Promise 객체로 수정

--- frontend/app/api/todos/[todoId]/route.ts
+++ frontend/app/api/todos/[todoId]/route.ts
@@ -1,7 +1,7 @@
 
 import { NextResponse } from 'next/server';
 
-export async function PUT(request: Request, {params}: {params: {todoId: string}}) {
+export async function PUT(request: Request, {params}: {params: Promise<{todoId: string}>}) {
     const {todoId} = await params; // 비동기로 매개변수를 받는다.
     const body = await request.json();
     const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/todos/${todoId}`, {
@@ -15,7 +15,7 @@ export async function PUT(request: Request, {params}: {params: {todoId: string}}
     return NextResponse.json(result);
 }
 
-export async function DELETE(request: Request, {params}: {params: {todoId: string}}) {
+export async function DELETE(request: Request, {params}: {params: Promise<{todoId: string}>}) {
     const {todoId} = await params;
     const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/todos/${todoId}`, {
         method: 'DELETE'
